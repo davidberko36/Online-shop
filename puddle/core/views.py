01896226ac4d49254b19f0276bf94308325df32f
davidberko36 <130989471+davidberko36@users.noreply.github.com>
2024-02-29 13:06:39 +0000

    29/02/2024

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from item.models import Category, Item
+from item.models import Category, Item 
+from .forms import SignupForm
 
 # Create your views here.
 def index(request):
@@ -14,3 +15,11 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+
+def signup(request):
+    form = SignupForm()
+
+    return render(request, 'core/signup.html',{
+        'form': form
+    })
